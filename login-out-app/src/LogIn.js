f91f38d193225db8df1cb6801eef0cb074a057a0
midnightsnidget <natalia.cowie@gmail.com>
2023-08-15 00:18:30 +0100

    Create variables for username and password

--- login-out-app/src/LogIn.js
+++ login-out-app/src/LogIn.js
@@ -1,1 +1,8 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+}
