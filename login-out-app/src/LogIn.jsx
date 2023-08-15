0d49824f29e0571ac1998f1db281899b22e43e01
midnightsnidget <natalia.cowie@gmail.com>
2023-08-15 01:32:57 +0100

    changed to login.jsx

---
+++ login-out-app/src/LogIn.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+
+    const handleLogin = () =>{
+        if (username === 'correctUsername' && password === 'correctPassword') {
+            history.push('/welcome');
+        } else {
+            history.push('unsuccessful');
+        }
+
+     };
+
+    return (
+        <div>
+            <h1>User Login</h1>
+            
+            <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            />
+
+            <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            />
+
+            <button onClick={handleLogin}>
+                Login
+            </button>
+
+        </div>
+    );
+}
+
+export default Login;
