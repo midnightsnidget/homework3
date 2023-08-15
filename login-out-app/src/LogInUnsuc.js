7a7b9efb37257f0a5958f07444aabd3786f3882b
midnightsnidget <natalia.cowie@gmail.com>
2023-08-15 01:06:27 +0100

    Altered function
    changed the name of the function and removed usestates and username and password information

--- login-out-app/src/LogInUnsuc.js
+++ login-out-app/src/LogInUnsuc.js
@@ -1,17 +1,11 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+function LogInUnsuc() {
     const history = useHistory();
 
-    const handleLogin = () =>{
-        if (username === 'correctUsername' && password === 'correctPassword') {
-            history.push('/welcome');
-        } else {
-            history.push('unsuccessful');
-        }
+    const handleGoToLogin = () =>{
+        
 
      };
 
